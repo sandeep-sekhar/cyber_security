0f42601ca2d4b8cc260234c2aa524419777a237c
sandeep-sekhar <sekharsandeep4@gmail.com>
2025-01-19 03:11:15 +0530

    model train done

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from cybersecurity.components.data_ingestion import DataIngestion
 from cybersecurity.components.data_validation import DataValidation
 from cybersecurity.components.data_transformation import DataTransformation
+from cybersecurity.components.model_trainer import ModelTrainer
 from cybersecurity.exception.exception import cyberSecurityException
 from cybersecurity.logging.logger import logging
 import sys
-from cybersecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from cybersecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from cybersecurity.entity.config_entity import TrainingPipelineConfig
 if __name__=='__main__':
     try:
@@ -29,5 +30,12 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
     except Exception as e:
         raise cyberSecurityException(e,sys)

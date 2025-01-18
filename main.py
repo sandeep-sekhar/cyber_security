28e66102ff0d80d9f4adbfc4c86714ce263efa6b
sandeep-sekhar <sekharsandeep4@gmail.com>
2025-01-18 17:24:05 +0530

    data validation done

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from cybersecurity.components.data_ingestion import DataIngestion
+from cybersecurity.components.data_validation import DataValidation
 from cybersecurity.exception.exception import cyberSecurityException
 from cybersecurity.logging.logger import logging
 import sys
-from cybersecurity.entity.config_entity import DataIngestionConfig
+from cybersecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from cybersecurity.entity.config_entity import TrainingPipelineConfig
 if __name__=='__main__':
     try:
@@ -11,6 +12,15 @@ if __name__=='__main__':
         data_ingestion= DataIngestion(dataingestionconfig)
         logging.info("initiating the data_ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info('data ingestion done')
         print(dataingestionartifact)
+        
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
+        
     except Exception as e:
         raise cyberSecurityException(e,sys)

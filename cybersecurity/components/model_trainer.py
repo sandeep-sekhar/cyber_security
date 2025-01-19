ad6bf5d3eb46aeffe3d8233fec306248b59db95d
sandeep-sekhar <sekharsandeep4@gmail.com>
2025-01-19 16:15:37 +0530

    mlflow compared

--- cybersecurity/components/model_trainer.py
+++ cybersecurity/components/model_trainer.py
@@ -23,7 +23,7 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier,
 )
-
+import mlflow
 
 
 class ModelTrainer:
@@ -33,6 +33,20 @@ class ModelTrainer:
             self.data_transformation_artifact=data_transformation_artifact
         except Exception as e:
             raise cyberSecurityException(e,sys)
+        
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+            
 
         
     def train_model(self,X_train,y_train,x_test,y_test):
@@ -47,7 +61,7 @@ class ModelTrainer:
             "Decision Tree": {
                 'criterion':['gini', 'entropy', 'log_loss'],
                 'splitter':['best','random'],
-                'max_features':['sqrt','log2'],
+                #'max_features':['sqrt','log2'],
             },
             "Random Forest":{
                 'criterion':['gini', 'entropy', 'log_loss'],
@@ -56,7 +70,7 @@ class ModelTrainer:
                 'n_estimators': [8,16,32,128,256]
             },
             "Gradient Boosting":{
-                'loss':['log_loss', 'exponential'],
+                #'loss':['log_loss', 'exponential'],
                 'learning_rate':[.1,.01,.05,.001],
                 'subsample':[0.6,0.7,0.75,0.85,0.9],
                 'criterion':['squared_error', 'friedman_mse'],
@@ -87,13 +101,13 @@ class ModelTrainer:
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
         
         ## Track the experiements with mlflow
-        #self.track_mlflow(best_model,classification_train_metric)
+        self.track_mlflow(best_model,classification_train_metric)
 
 
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
 
-        #self.track_mlflow(best_model,classification_test_metric)
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             
